f6d8f51e8a522f9422f94650c73f801a301655e0
janani-ravikumar <88891844+janani-ravikumar@users.noreply.github.com>
2022-02-22 15:56:18 +0530

    added service registration in module file

--- apm/src/app/user/user.module.ts
+++ apm/src/app/user/user.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { UserComponent } from './user.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { UserService } from './user.service';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,9 @@ import { RouterModule } from '@angular/router';
       { path: 'login', component: LoginComponent},
       { path: 'edit', component: UserComponent}
     ])
+  ],
+  providers: [
+    UserService
   ]
 })
 export class UserModule { }
--- apm/src/app/user/user.service.ts
+++ apm/src/app/user/user.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 import { IUser } from './user';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 
 export class UserService {
 
