c6fb74258725da4604f7f4a3f6d26b50ae4d90e3
janani-ravikumar <88891844+janani-ravikumar@users.noreply.github.com>
2022-02-28 18:41:04 +0530

    added tests for nav and welcome component

---
+++ apm/src/app/welcome/navigation.component.spec.ts
@@ -0,0 +1,54 @@
+import { DebugElement } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing"
+import { UserService } from "../user/user.service";
+import { NavigationComponent } from "./navigation.component"
+
+describe('NavigationComponent', () => {
+
+    let fixture: ComponentFixture<NavigationComponent>,
+        debugElement: DebugElement,
+        component: NavigationComponent,
+        userServiceStub: Partial<UserService>,
+        userService: any;
+
+    beforeEach(() => {
+        userServiceStub = jasmine.createSpyObj('UserService', ['isAuthenticated']);
+
+        TestBed.configureTestingModule({
+            declarations: [
+                NavigationComponent
+            ],
+            providers: [
+                { provide: UserService, useValue: userServiceStub }
+            ]
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(NavigationComponent);
+        component = fixture.componentInstance;
+        debugElement = fixture.debugElement;
+        fixture.detectChanges();
+        userService = TestBed.inject(UserService);
+    });
+
+    it('should have the page Title - My Angular App', () => {
+        expect(component.pageTitle).toEqual('My Angular App');
+    });
+
+    it('should display login link if user is not aunthenticated', () => {
+        userService.isAuthenticated.and.returnValue(false);
+        fixture.detectChanges();
+
+        const link = debugElement.nativeElement.querySelectorAll('.nav-link');
+
+        expect(link[link.length - 1].text).toBe('Login');
+    })
+
+    it('should display Edit profile menu if user is aunthenticated', () => {
+        userService.isAuthenticated.and.returnValue(true);
+        fixture.detectChanges();
+
+        const link = debugElement.nativeElement.querySelectorAll('.nav-link');
+
+        expect(link[link.length - 1].text).toBe('Edit Profile');
+    })
+})
